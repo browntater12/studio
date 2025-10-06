77804ae5d6f4072656fb2b76f40c940e7a0abcde
Conner Brown <BrownConner15@gmail.com>
2025-10-06 15:53:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3294958671-4c5c0",
+  "appId": "1:365425048497:web:a0f5e7fbd1e51e1c809b90",
+  "apiKey": "AIzaSyD5-jFvpFbH65Ok1YPRo1IN3OEn7XybXHQ",
+  "authDomain": "studio-3294958671-4c5c0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "365425048497"
+};
