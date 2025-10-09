4307c91cdddea34e3ac101fbd4ce4a6ce795072e
Conner Brown <BrownConner15@gmail.com>
2025-10-09 14:33:09 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Missing or insuffi

--- src/components/main-sidebar.tsx
+++ src/components/main-sidebar.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import * as React from 'react';
@@ -26,7 +27,6 @@ import { DbStatus } from './db-status';
 
 interface MainSidebarProps {
   accounts: Account[];
-  products: Product[];
   isLoading: boolean;
 }
 
